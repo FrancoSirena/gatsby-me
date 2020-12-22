f3d085e32ce69b1dca28c128d360cda678918c09
Franco Sirena <franco.sirena@bitsighttech.com>
2020-12-22 18:19:24 +0100

    Fix icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.ico`,
+        icon: `src/images/favicon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
