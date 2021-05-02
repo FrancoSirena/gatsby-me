2e09418104dbe62cd9218a0befe09c74fe3bfa55
Franco Sirena <fsirena@gmail.com>
2021-05-02 14:38:29 +0200

    Start GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,8 +61,17 @@ module.exports = {
         icon: `src/images/icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-4XWJNQ1VCB",
+        ],
+        pluginConfig: {
+          head: false,
+          respectDNT: true,
+        },
+      },
+    },
   ],
 }
